3fe5498e15bd37126801626632fb33d885354451
taylersala <taylersala@gmail.com>
2024-08-05 14:16:23 -0700

    5

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
 // DO YOUR MAGIC
+const express = require('express');
+const Car = require('./cars-model')
+const router = express.Router()
+const {
+    checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid
+} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('posting new car')
+})
+
+module.exports = router;
