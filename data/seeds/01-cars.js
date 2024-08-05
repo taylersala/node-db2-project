975c9d862462196bd7d062b2789ec31234ee974a
taylersala <taylersala@gmail.com>
2024-08-05 12:35:41 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+    {
+        vin: '111111',
+        make: 'toyota',
+        model: 'solara',
+        mileage: 100000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '222222',
+        make: 'toyota',
+        model: 'corola',
+        mileage: 200000,
+        title: 'salvage',
+    },
+    {
+        vin: '234254',
+        make: 'honda',
+        model: 'something',
+        mileage: 200000,
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars').insert(cars)
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
